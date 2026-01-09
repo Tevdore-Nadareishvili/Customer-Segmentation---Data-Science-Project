e67c3d004aa3a3fb78a00626d78aed0fc15cc943
Tevdore-Nadareishvili <nadareishvili.tevdor@kiu.edu.ge>
2026-01-09 18:05:44 +0400

    added DataProcessor class for hanlding data loading, cleaning and feature engineering

--- final_project.py
+++ final_project.py
@@ -13,3 +13,18 @@ import seaborn as sns
 sns.set(style="whitegrid")
 pd.set_option('display.max_columns', None)
 RANDOM_STATE = 42
+
+
+# ==========================================
+# 1. DATA LOADING & PROCESSING CLASS
+# ==========================================
+class DataProcessor:
+    """
+    Handles data loading, cleaning, and feature engineering.
+    """
+    
+    def __init__(self, url=None, filepath=None):
+        self.url = url
+        self.filepath = filepath
+        self.df = None
+        self.rfm = None
